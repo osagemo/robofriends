3be9d9e6ec4131cb39595386dec359cc882c4d0d
Osagemo <osagemo@gmail.com>
2020-03-16 18:56:59 +0000

    use redux for state management and add logger

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { setSearchField, fetchRobots } from "../actions";
+import { connect } from "react-redux";
 import SimpleBar from "simplebar-react";
 import "simplebar/dist/simplebar.min.css";
 import ErrorBoundry from "../components/ErrorBoundry";
@@ -6,45 +8,54 @@ import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import "./App.css";
 
-export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { robots: [], searchField: "" };
-    }
+const mapStateToProps = state => ({
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
+});
 
-    componentDidMount() {
-        fetch("https://jsonplaceholder.typicode.com/users")
-            .then(response => {
-                return response.json();
-            })
-            .then(users => {
-                this.setState({ robots: users });
-            });
-    }
+const mapDispatchToProps = dispatch => ({
+    onSearchChange: event => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(fetchRobots())
+});
 
-    onSearchChange = event => {
-        this.setState({ searchField: event.target.value });
-    };
-    render() {
-        const { robots, searchField } = this.state;
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchField);
-        });
-
-        if (!robots.length) {
-            return <h1>Fetching users...</h1>;
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(
+    class App extends React.Component {
+        componentDidMount() {
+            this.props.onRequestRobots();
         }
 
-        return (
-            <div className="tc">
-                <h1 className="f1">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <SimpleBar style={{ maxHeight: "75vh" }}>
-                    <ErrorBoundry>
-                        <CardList robots={filteredRobots} />
-                    </ErrorBoundry>
-                </SimpleBar>
-            </div>
-        );
+        render() {
+            const {
+                searchField,
+                onSearchChange,
+                robots,
+                isPending
+            } = this.props;
+
+            const filteredRobots = robots.filter(robot => {
+                return robot.name.toLowerCase().includes(searchField);
+            });
+
+            if (isPending) {
+                return <h1>Fetching users...</h1>;
+            }
+
+            return (
+                <div className="tc">
+                    <h1 className="f1">RoboFriends</h1>
+                    <SearchBox searchChange={onSearchChange} />
+                    <SimpleBar style={{ maxHeight: "75vh" }}>
+                        <ErrorBoundry>
+                            <CardList robots={filteredRobots} />
+                        </ErrorBoundry>
+                    </SimpleBar>
+                </div>
+            );
+        }
     }
-}
+);
