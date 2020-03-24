5f861c53a80dc25807d8fbfb4c93939f2636da40
Osagemo <osagemo@gmail.com>
2020-03-24 12:16:23 +0000

    Update descriptions

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunkMiddleware from "redux-thunk";
-import { createLogger } from "redux-logger";
+// import { createLogger } from "redux-logger";
 import { searchRobots, requestRobots } from "./reducers";
 import "tachyons";
 import "./index.css";
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "development") {
   });
 }
 
-const logger = createLogger();
+// const logger = createLogger();
 const rootReducer = combineReducers({ searchRobots, requestRobots });
 
 const store = createStore(
