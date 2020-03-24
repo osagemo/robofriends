5dba6d3ad1a13a5b31bff875107e19eed5b3c4bd
Osagemo <osagemo@gmail.com>
2020-03-24 13:52:31 +0000

    Add aria-label for accessability score

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -5,6 +5,7 @@ export default function SearchBox({ searchField, searchChange }) {
   return (
     <div className="pa2">
       <input
+        aria-label="Search robots"
         className="pa3 ba b--green bg-lightest-blue"
         type="search"
         placeholder="search robots"
